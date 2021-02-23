a882bdba78e8d31777411168f7071a5b115eb957
Stacie Taitague <stacie.taitague@gmail.com>
2021-02-23 18:49:56 -0500

    Tue. 2/23 Babys First Rails Homework

---
+++ app/controllers/api/welcomes_controller.rb
@@ -0,0 +1,13 @@
+class Api::WelcomesController < ApplicationController
+  def hello
+    @random = Random.new.rand(100)
+    render 'hello.json.jb'
+  end
+
+  def about
+    @code = "ruby"
+    render 'about.json.jb'
+  end
+    
+
+end
